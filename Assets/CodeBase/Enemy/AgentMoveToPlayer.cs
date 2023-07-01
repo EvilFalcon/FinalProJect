6a94809293a4f216b9e8fbacdcaefbdf0e754205
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2023-07-01 08:10:10 +0300

    Del Dragon prefab

--- Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
+++ Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
@@ -3,12 +3,30 @@ using UnityEngine.AI;
 
 namespace CodeBase.Enemy
 {
-    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(
+        typeof(NavMeshAgent))]
     public class AgentMoveToPlayer : MonoBehaviour
     {
         private const float MinimalDistance = 1;
         private NavMeshAgent _agent;
         private Transform _heroTransform;
+        private HeroLocator _locator;
+
+        private void Awake()
+        {
+            _locator = GetComponentInChildren<HeroLocator>();
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void OnEnable()
+        {
+            _locator.FoundHero += InitializeHeroTransform;
+        }
+
+        private void OnDisable()
+        {
+            _locator.FoundHero -= InitializeHeroTransform;
+        }
 
         private void Update()
         {
@@ -17,8 +35,21 @@ namespace CodeBase.Enemy
 
         private void HeroNotReached()
         {
-            if (Vector3.Distance(_agent.transform.position, _heroTransform.position) <= MinimalDistance)
-                _agent.destination = _heroTransform.position;
+
+            if (_heroTransform != null)
+            {
+            Debug.Log(Vector3.Distance(_agent.transform.position, _heroTransform.position) >= MinimalDistance);
+               // if (Vector3.Distance(_agent.transform.position, _heroTransform.position) >= MinimalDistance)
+               // {
+                    Debug.Log("я в оп");
+                    _agent.destination = _heroTransform.position;
+                //}
+            }
+        }
+
+        private void InitializeHeroTransform()
+        {
+            _heroTransform = _locator.transform;
         }
     }
 }
---
+++ Assets/CodeBase/Enemy/HeroLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class HeroLocator : MonoBehaviour
+    {
+        public event Action FoundHero;
+        public Transform HeroTransform { get; private set; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out global::Hero hero))
+            {
+                HeroTransform = hero.transform;
+                FoundHero?.Invoke();
+            }
+        }
+    }
+}
